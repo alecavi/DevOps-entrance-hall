d876ee89db9df2139fd308bb51a8676756e18801
alecavi <acavicchioli@dundee.ac.uk>
2023-01-06 17:27:41 +0100

    buttons should now stay synced - 11

--- src/static/toggle_switch.js
+++ src/static/toggle_switch.js
@@ -1,18 +1,18 @@
-let buttons = new Map()
+let like_buttons = new Map()
 
-function store_button(key, value) {
-    let list = buttons.get(key);
+function store_like_button(key, value) {
+    let list = like_buttons.get(key);
     if (list === undefined) {
-        buttons.set(key, [value]);
+        like_buttons.set(key, [value]);
     } else {
         list.push(value)
     }
-    console.log("store_button ", JSON.stringify(buttons));
+    console.log("store_button ", JSON.stringify(like_buttons));
 }
 
 window.addEventListener("DOMContentLoaded", function () {
     for (const button of document.getElementsByClassName("like-button")) {
-        store_button(button.dataset.uuid, button);
+        store_like_button(button.dataset.uuid, button);
 
         button.addEventListener("change", async function () {
             await fetch("http://35.217.17.201/api/like", {
@@ -22,8 +22,8 @@ window.addEventListener("DOMContentLoaded", function () {
             })
             console.log("checked: ", this.checked);
             console.log("uuid: ", this.dataset.uuid);
-            console.log("buttons", JSON.stringify(buttons));
-            for (const button of buttons[this.dataset.uuid]) {
+            console.log("buttons", JSON.stringify(like_buttons));
+            for (const button of like_buttons[this.dataset.uuid]) {
                 button.checked = this.checked;
             }
         })
@@ -31,18 +31,16 @@ window.addEventListener("DOMContentLoaded", function () {
 
     for (const button of document.getElementsByClassName("watch-later-button")) {
         button.addEventListener("change", async function () {
-            store_button(button.dataset.uuid, this);
-
             await fetch("http://35.217.17.201/api/watch-later", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ state: this.checked, video_id: this.dataset.uuid })
             })
-            for (const button of buttons[this.dataset.uuid]) {
+            for (const button of like_buttons[this.dataset.uuid]) {
                 button.checked = this.checked;
             }
         })
     }
-    console.log("layer one", JSON.stringify(buttons));
+    console.log("layer one", JSON.stringify(like_buttons));
 })
-console.log("layer zero", JSON.stringify(buttons));
+console.log("layer zero", JSON.stringify(like_buttons));
