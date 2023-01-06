8f1261b7117017217b984109b81af6cd2860ca39
alecavi <acavicchioli@dundee.ac.uk>
2023-01-06 13:07:15 +0100

    fixed duplicate http

--- src/static/toggle_switch.js
+++ src/static/toggle_switch.js
@@ -1,6 +1,6 @@
 for (const button of document.getElementsByClassName("like-button")) {
     button.addEventListener("change", async function () {
-        await fetch("http://http://35.217.17.201/like", {
+        await fetch("http://35.217.17.201/like", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ state: this.checked, video: this.dataset.uuid })
