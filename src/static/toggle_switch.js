53b0588b63052797dfe19ece6d675a0c35435822
alecavi <acavicchioli@dundee.ac.uk>
2023-01-06 13:17:52 +0100

    removed debug prints

--- src/static/toggle_switch.js
+++ src/static/toggle_switch.js
@@ -1,8 +1,6 @@
 window.addEventListener("DOMContentLoaded", (_) => {
     for (const button of document.getElementsByClassName("like-button")) {
-        console.log("ADDING", button)
         button.addEventListener("change", async function () {
-            console.log("TRIGGERED", this)
             await fetch("http://35.217.17.201/like", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
