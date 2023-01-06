4af9c55ebd7ff53292be991b03ce88d6d9b534ba
alecavi <acavicchioli@dundee.ac.uk>
2023-01-06 16:41:09 +0100

    buttons should now stay synced

--- src/static/toggle_switch.js
+++ src/static/toggle_switch.js
@@ -1,24 +1,42 @@
-// FIXME: multiple toggles can refer to the same videos, since the same video can appear repeatedly. 
-// Ideally, those toggles should stay in sync
+let buttons = new Map()
+
+function store_button(key, value) {
+    let list = buttons.get(key);
+    if (list === undefined) {
+        list = [value];
+    } else {
+        list.push(value)
+    }
+}
 
 window.addEventListener("DOMContentLoaded", (_) => {
     for (const button of document.getElementsByClassName("like-button")) {
+        store_button(this.dataset.uuid, this);
+
         button.addEventListener("change", async function () {
             await fetch("http://35.217.17.201/api/like", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ state: this.checked, video_id: this.dataset.uuid })
             })
+            for (const button of buttons[this.dataset.uuid]) {
+                button.checked = this.checked;
+            }
         })
     }
 
     for (const button of document.getElementsByClassName("watch-later-button")) {
         button.addEventListener("change", async function () {
+            store_button(this.dataset.uuid, this);
+
             await fetch("http://35.217.17.201/api/watch-later", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ state: this.checked, video_id: this.dataset.uuid })
             })
+            for (const button of buttons[this.dataset.uuid]) {
+                button.checked = this.checked;
+            }
         })
     }
 
