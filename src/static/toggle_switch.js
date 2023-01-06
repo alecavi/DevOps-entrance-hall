3e0d27bfe38c59f55ad64ca280663aa7ca3def04
alecavi <acavicchioli@dundee.ac.uk>
2023-01-06 13:16:11 +0100

    hopefully added a working like button - 5

--- src/static/toggle_switch.js
+++ src/static/toggle_switch.js
@@ -1,12 +1,13 @@
-console.log("RUNNING")
-for (const button of document.getElementsByClassName("like-button")) {
-    console.log("ADDING", button)
-    button.addEventListener("change", async function () {
-        console.log("TRIGGERED", this)
-        await fetch("http://35.217.17.201/like", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ state: this.checked, video: this.dataset.uuid })
+window.addEventListener("DOMContentLoaded", (_) => {
+    for (const button of document.getElementsByClassName("like-button")) {
+        console.log("ADDING", button)
+        button.addEventListener("change", async function () {
+            console.log("TRIGGERED", this)
+            await fetch("http://35.217.17.201/like", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ state: this.checked, video: this.dataset.uuid })
+            })
         })
-    })
-}
+    }
+})
